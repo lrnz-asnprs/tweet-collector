d55ac621158991088ffe53c39aa169513a90f35d
Laurenz Aisenpreis <la@halfspace.io>
2022-09-30 12:59:11 +0200

    twtiter user class

--- tweet_collector/utils/TwitterUser.py
+++ tweet_collector/utils/TwitterUser.py
@@ -1,14 +1,42 @@
 
 
 
-class TwitterUser:
-    def __init__(self, user_id, screen_name, followers_count, friends_count, verified, location):
+from datetime import datetime
+from typing import List
 
+
+class TwitterUser:
+    def __init__(
+        self, 
+        user_id: int, 
+        user_name: str, 
+        followers_count: int, 
+        friends_count: int, 
+        tweet_count: int,
+        verified: bool, 
+        location: str,
+        created_at: datetime,
+        fake_news_tweets: List[int] = None,
+        following_ids: List[int] = None
+        ):
+        """
+        Twitter user object holding all relevant attributes.
+        """
         self.user_id = user_id,
-        self.screen_name = screen_name,
+        self.user_name = user_name,
         self.followers_count = followers_count,
         self.friends_count = friends_count,
+        self.tweet_count = tweet_count,
         self.verified = verified,
-        self.location = location
+        self.location = location,
+        self.created_at = created_at,
+        self.fake_news_tweets = fake_news_tweets,
+        self.following_ids = following_ids
+        
+    def is_verified(self):
+        return self.verified
+
+    def get_fake_news_tweets(self):
+        return self.fake_news_tweets
     
     
