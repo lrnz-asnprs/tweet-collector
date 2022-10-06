34620bdddd71bd38831b17f632848b2a094cc37c
Laurenz Aisenpreis <la@halfspace.io>
2022-10-06 18:21:46 +0200

    user collection and falsity scores

--- fake_collector/modules/user_profile_collector.py
+++ fake_collector/modules/user_profile_collector.py
@@ -1,10 +1,13 @@
+import sys
+sys.path.append("/Users/laurenzaisenpreis/Uni/Thesis/tweet-collector")
+
 from fake_collector.utils.TwythonConnector import TwythonConnector
 import os
 import json 
 import pickle
 from fake_collector.configs.directory_config import Directories
 from fake_collector.utils.TwitterUser import TwitterUser
-from typing import List
+from typing import Dict, List
 import pandas as pd
 
 class UserProfileCollector:
@@ -26,41 +29,76 @@ class UserProfileCollector:
         # iterate over fake news tweet files in that directory
         #TODO might remove unnecessary loop and create user profiles immediately when loading/reading
         print("Reading tweets from files")
+        # iterate over files in that directory
         tweets = []
         for filename in os.listdir(path):
-            if filename.startswith("0"):
-                file = open(path / filename, 'r')
+            if filename.startswith('0'):
+                print("Try file ", filename)
+                items = filename.split("_")
                 for line in open(path / filename, 'r'):
-                    tweets.append(json.loads(line))
+                    tweets.append((items[2],items[3],json.loads(line)))
 
         # get user information
         print("Creating user objects from tweets")
         users = {}
         for tweet in tweets:
             try:
-                for user in tweet["includes"]["users"]:
-                    user_id = int(user["id"])
-                    if user_id not in users:
-                        users[user_id] = TwitterUser(user_id=user_id, user_name=user["name"], followers_count=user["public_metrics"]["followers_count"], friends_count=user["public_metrics"]["following_count"], tweet_count=user["public_metrics"]["tweet_count"], verified=user["verified"], created_at=user["created_at"])
+                if 'includes' in tweet[2].keys():
+                    for user in tweet[2]["includes"]["users"]:
+                        user_id = user["id"]
+                        if user_id not in users:
+                            users[user_id] = TwitterUser(user_id=user_id, user_name=user["name"], description=user['description'], followers_count=user["public_metrics"]["followers_count"], friends_count=user["public_metrics"]["following_count"], tweet_count=user["public_metrics"]["tweet_count"], verified=user["verified"], created_at=user["created_at"])
             except:
-                print("Problem with creating user profile in tweet ", tweet)
+                print("Problem with tweet user", user)
 
         # add fake tweet ids to the corresponding user profile
-        print("Adding fake tweet ids to user profiles")
-        for tweet in tweets:
+        print("Adding ids to user profiles")
+        for entry in tweets:
+
+            topic = entry[0]
+            label = entry[1]
+            if label == 'barely-true':
+                label = 'mostly-false' # https://www.politifact.com/article/2011/jul/27/-barely-true-mostly-false/
+
             try:
-                for tweet in tweet["data"]:
-                    # get corresponding user
-                    author_id = int(tweet["author_id"])
-                    tweet_id = int(tweet["id"])
+                if "data" in entry[2].keys():
+                    for tweet in entry[2]["data"]:
+                        # get corresponding user
+                        author_id = tweet["author_id"]
+                        tweet_id = tweet["id"]
+                        # get type if present
+                        if 'referenced_tweets' in tweet.keys():
+                            tweet_type = tweet['referenced_tweets'][0]['type']
+                        else:
+                            tweet_type = 'tweet'
+                        # add tweet info to user
 
-                    user_obj = users.get(author_id)
-                    user_obj.add_fake_news_tweet(tweet_id)
+                        user_obj = users.get(author_id)
+
+                        match tweet_type:
+                            case "tweet":
+                                if topic not in user_obj.tweets[label].keys():
+                                    user_obj.tweets[label][topic] = [tweet_id]
+                                else:
+                                    user_obj.tweets[label][topic].append(tweet_id)
+                            case "retweeted":
+                                if topic not in user_obj.retweets[label].keys():
+                                    user_obj.retweets[label][topic] = [tweet_id]
+                                else:
+                                    user_obj.retweets[label][topic].append(tweet_id)
+                            case "replied_to":
+                                if topic not in user_obj.replies[label].keys():
+                                    user_obj.replies[label][topic] = [tweet_id]
+                                else:
+                                    user_obj.replies[label][topic].append(tweet_id)
             except:
-                print("Problem with adding tweet to user profile for  ", tweet)
+                print("issue with tweet ", tweet)
 
-        
-        # store list of TwitterUsers as picke file
+
+        print("Calculating falsity scores for users")
+        for user in users.values():
+            user.calculate_falsity_score()
+
         print("Saving TwitterUsers list as pickle file")
         users_list = list(users.values())
         path = directory.USERS_PATH
@@ -70,7 +108,7 @@ class UserProfileCollector:
             pickle.dump(users_list, f)
 
 
-    def load_user_profiles(self) -> List[TwitterUser]:
+    def load_user_profiles_as_list(self) -> List[TwitterUser]:
         # Open pickle file
         directories = Directories()
         path = directories.USERS_PATH
@@ -88,9 +126,27 @@ class UserProfileCollector:
             users_loaded = pickle.load(f)
 
         # Turn into dict
-        users_dict = {user.user_id:user.get_user_as_dict() for user in users_loaded}
+        users_dict = {user.user_id : user.get_user_as_dict() for user in users_loaded}
 
         users_df = pd.DataFrame.from_dict(users_dict.values())
     
         return users_df
 
+    def load_users_profiles_as_dict(self) -> Dict[str, TwitterUser]:
+        # Open pickle file
+        directories = Directories()
+        path = directories.USERS_PATH
+        filename = "users.pickle"
+        with open(path / filename, "rb") as f:
+            users_loaded = pickle.load(f)
+
+        # Turn into dict
+        users_dict = {user.user_id : user for user in users_loaded}
+    
+        return users_dict
+
+
+
+if __name__ == "__main__":
+    user_collector = UserProfileCollector()
+    user_collector.save_user_profiles()
--- fake_collector/utils/TwitterUser.py
+++ fake_collector/utils/TwitterUser.py
@@ -1,35 +1,62 @@
 
-
-
 from datetime import datetime
-from typing import List
+from typing import Dict, List
 
 
 class TwitterUser:
     def __init__(
         self, 
-        user_id: int, 
-        user_name: str, 
+        user_id: str, 
+        user_name: str,
+        description: str,
         followers_count: int, 
         friends_count: int, 
         tweet_count: int,
         verified: bool, 
         created_at: datetime,
-        fake_news_tweets: List[int] = None,
-        following_ids: List[int] = None
+        average_falsity_score: int = None,
+        aggregate_falsity_score: int = None,
+        tweets: Dict[str, Dict[str,List]] = None,
+        retweets: Dict[str, Dict[str,List]] = None,
+        replies: Dict[str, Dict[str,List]] = None,
         ):
         """
         Twitter user object holding all relevant attributes.
         """
         self.user_id = user_id
         self.user_name = user_name
+        self.description = description
         self.followers_count = followers_count
         self.friends_count = friends_count
         self.tweet_count = tweet_count
         self.verified = verified
         self.created_at = created_at
-        self.fake_news_tweets = [] # empty at first
-        self.following_ids = []
+        self.tweets = {
+            "pants-fire": {}, # categories in the next level
+            "false": {},
+            "mostly-false": {},
+            "half-true": {},
+            "mostly-true": {},
+            "true": {}
+        } 
+        self.retweets = {
+            "pants-fire": {},
+            "false": {},
+            "mostly-false": {},
+            "half-true": {},
+            "mostly-true": {},
+            "true": {}
+        } 
+        self.replies = {
+            "pants-fire": {},
+            "false": {},
+            "mostly-false": {},
+            "half-true": {},
+            "mostly-true": {},
+            "true": {}
+        } 
+        self.aggregate_falsity_score = -1 # initialized at -1 
+        self.average_falsity_score = -1 # initialized at -1 
     
     def get_user_id(self):
         return self.user_id
@@ -37,15 +64,179 @@ class TwitterUser:
     def is_verified(self):
         return self.verified
 
-    def get_fake_news_tweets(self):
-        return self.fake_news_tweets
+    # All tweet getters
+    def get_all_false_tweets_retweets(self):
+        all_false_tweets = self.get_false_tweets() + self.get_mostly_false_tweets() + self.get_pants_fire_tweets()
+        all_false_retweets = self.get_false_retweets() + self.get_mostly_false_retweets() + self.get_pants_fire_retweets()
+        return all_false_tweets + all_false_retweets
 
-    def add_following_ids(self, following_ids: List[int]):
+    def get_all_true_tweets_retweets(self):
+        all_true_tweets = self.get_true_tweets() + self.get_mostly_true_tweets() + self.get_half_true_tweets()
+        all_true_retweets = self.get_true_retweets() + self.get_mostly_true_retweets() + self.get_half_true_retweets()
+        return all_true_tweets + all_true_retweets
+
+    # falsity score
+    def calculate_falsity_score(self):
+
+        temp_falsity_score = 0
+        tweet_counter = 0
+
+        # Tweets
+        for label in self.tweets.keys():
+            match label:
+                case "pants-fire":
+                    pants_tweets = []
+                    for topic in self.tweets['pants-fire']:
+                        pants_tweets = pants_tweets + self.tweets['pants-fire'][topic]
+                    temp_falsity_score += len(pants_tweets) * 1
+                    tweet_counter += len(pants_tweets)
+                case "false":
+                    false_tweets = self.get_false_tweets()
+                    temp_falsity_score += len(false_tweets) * 0.8
+                    tweet_counter += len(false_tweets)
+                case "mostly-false":
+                    mostly_false_tweets = []
+                    for topic in self.tweets['mostly-false']:
+                        mostly_false_tweets = mostly_false_tweets + self.tweets['mostly-false'][topic]
+                    temp_falsity_score += len(mostly_false_tweets) * 0.6
+                    tweet_counter += len(mostly_false_tweets)
+                case "half-true":
+                    half_true_tweets = []
+                    for topic in self.tweets['half-true']:
+                        half_true_tweets = half_true_tweets + self.tweets['half-true'][topic]
+                    temp_falsity_score += len(half_true_tweets) * 0.4
+                    tweet_counter += len(half_true_tweets)
+                case "mostly-true":
+                    mostly_true_tweets = []
+                    for topic in self.tweets['mostly-true']:
+                        mostly_true_tweets = mostly_true_tweets + self.tweets['mostly-true'][topic]
+                    temp_falsity_score += len(mostly_true_tweets) * 0.2
+                    tweet_counter += len(mostly_true_tweets)
+                case "true":
+                    true_tweets = self.get_true_tweets()
+                    tweet_counter += len(true_tweets)
+        
+        # Retweets
+        for label in self.retweets.keys():
+            match label:
+                case "pants-fire":
+                    pants_retweets = []
+                    for topic in self.retweets['pants-fire']:
+                        pants_retweets = pants_retweets + self.retweets['pants-fire'][topic]
+                    temp_falsity_score += len(pants_tweets) * 1
+                    tweet_counter += len(pants_retweets)
+                case "false":
+                    false_retweets = self.get_false_retweets()
+                    temp_falsity_score += len(false_retweets) * 0.8
+                    tweet_counter += len(false_retweets)
+                case "mostly-false":
+                    mostly_false_retweets = []
+                    for topic in self.retweets['mostly-false']:
+                        mostly_false_retweets = mostly_false_retweets + self.retweets['mostly-false'][topic]
+                    temp_falsity_score += len(mostly_false_retweets) * 0.6
+                    tweet_counter += len(mostly_false_retweets)
+                case "half-true":
+                    half_true_retweets = []
+                    for topic in self.retweets['half-true']:
+                        half_true_retweets = half_true_retweets + self.retweets['half-true'][topic]
+                    temp_falsity_score += len(half_true_retweets) * 0.4
+                    tweet_counter += len(half_true_retweets)
+                case "mostly-true":
+                    mostly_true_retweets = []
+                    for topic in self.retweets['mostly-true']:
+                        mostly_true_retweets = mostly_true_retweets + self.retweets['mostly-true'][topic]
+                    temp_falsity_score += len(mostly_true_retweets) * 0.2
+                    tweet_counter += len(mostly_true_retweets)
+                case "true":
+                    true_retweets = self.get_true_retweets()
+                    tweet_counter += len(true_retweets)
+
+
+        if tweet_counter == 0: # user only had replies
+            self.aggregate_falsity_score = -1
+            self.average_falsity_score = -1
+        else:
+            self.aggregate_falsity_score = temp_falsity_score
+            self.average_falsity_score = temp_falsity_score / tweet_counter
+
+
+    def add_following_ids(self, following_ids: List[str]):
         self.following_ids = list(set(self.following_ids + following_ids))
-
-    def add_fake_news_tweet(self, tweet_id: int):
-        self.fake_news_tweets.append(tweet_id)
 
     def get_user_as_dict(self):
         return self.__dict__
+
+    # Tweet getters
+    def get_false_tweets(self):
+        fake_tweets = []
+        for topic in self.tweets['false']:
+            fake_tweets = fake_tweets + self.tweets['false'][topic]
+        return fake_tweets
+
+    def get_pants_fire_tweets(self):
+        pants_fire_tweets = []
+        for topic in self.tweets['pants-fire']:
+            pants_fire_tweets = pants_fire_tweets + self.tweets['pants-fire'][topic]
+        return pants_fire_tweets
+
+    def get_mostly_false_tweets(self):
+        mostly_false_tweets = []
+        for topic in self.tweets['mostly-false']:
+            mostly_false_tweets = mostly_false_tweets + self.tweets['mostly-false'][topic]
+        return mostly_false_tweets
+
+    def get_true_tweets(self):
+        true_tweets = []
+        for topic in self.tweets['true']:
+            true_tweets = true_tweets + self.tweets['true'][topic]
+        return true_tweets
+
+    def get_mostly_true_tweets(self):
+        mostly_true_tweets = []
+        for topic in self.tweets['mostly-true']:
+            mostly_true_tweets = mostly_true_tweets + self.tweets['mostly-true'][topic]
+        return mostly_true_tweets
+
+    def get_half_true_tweets(self):
+        half_true_tweets = []
+        for topic in self.tweets['half-true']:
+            half_true_tweets = half_true_tweets + self.tweets['half-true'][topic]
+        return half_true_tweets
+
+    # Retweet getters
+    def get_false_retweets(self):
+        fake_retweets = []
+        for topic in self.retweets['false']:
+            fake_retweets = fake_retweets + self.retweets['false'][topic]
+        return fake_retweets
+
+    def get_pants_fire_retweets(self):
+        pants_fire_retweets = []
+        for topic in self.retweets['pants-fire']:
+            pants_fire_retweets = pants_fire_retweets + self.retweets['pants-fire'][topic]
+        return pants_fire_retweets
+
+    def get_mostly_false_retweets(self):
+        mostly_false_retweets = []
+        for topic in self.retweets['mostly-false']:
+            mostly_false_retweets = mostly_false_retweets + self.retweets['mostly-false'][topic]
+        return mostly_false_retweets
+
+    def get_true_retweets(self):
+        true_retweets = []
+        for topic in self.retweets['true']:
+            true_retweets = true_retweets + self.retweets['true'][topic]
+        return true_retweets
+
+    def get_mostly_true_retweets(self):
+        mostly_true_retweets = []
+        for topic in self.retweets['mostly-true']:
+            mostly_true_retweets = mostly_true_retweets + self.retweets['mostly-true'][topic]
+        return mostly_true_retweets
+
+    def get_half_true_retweets(self):
+        half_true_retweets = []
+        for topic in self.retweets['half-true']:
+            half_true_retweets = half_true_retweets + self.retweets['half-true'][topic]
+        return half_true_retweets
     
