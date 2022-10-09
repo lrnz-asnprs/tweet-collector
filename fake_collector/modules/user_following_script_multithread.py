497ff0df0de23e6060aab2250a7ba918ba2cf9d4
Laurenz Aisenpreis <la@halfspace.io>
2022-10-09 17:41:27 +0200

    refactoring

--- fake_collector/modules/user_following_script_multithread.py
+++ fake_collector/modules/user_following_script_multithread.py
@@ -10,6 +10,7 @@ from typing import List
 from multiprocessing import Process
 from fake_collector.modules.user_profile_collector import UserProfileCollector
 from fake_collector.modules.user_following_collector_v2 import UserFollowingCollectorV2
+from fake_collector.modules.user_following_collector import UserFollowingCollector
 import queue
 import threading
 import time
@@ -34,35 +35,15 @@ def add_user_friends_ids(user_queue, app_type):
         else:
 
             twitter_app = TwythonConnector(app_type=app_type)
-
             twitter_app.make_connection()
 
             user = user_queue.get()
-
-            print(f"{app_type} app: trying user ", user.user_name)
-            cursor = -1
-            while cursor != 0:
-                try:
-                    print(f"{app_type} app getting response")
-                    response = twitter_app.twitter_connection.get_friends_ids(user_id=user.get_user_id(), cursor=cursor, count=5000)
-                    following_ids = response['ids']
-                    user.add_following_ids(following_ids)
-                    cursor = response['next_cursor']
-
-                except TwythonRateLimitError as error:
-                    rate_limit_stats = twitter_app.twitter_connection.get_application_rate_limit_status()
-                    remaining_time = rate_limit_stats["resources"]["friends"]["/friends/ids"]["reset"]
-                    remainder = remaining_time - time.time() # epoch time
-                    minutes = remainder // 60 % 60
-                    print(f"{app_type} app sleeping for minutes: ", minutes)
-                    time.sleep(remainder)
-                    print(f"{app_type} app resume connections")
-                    twitter_app.make_connection() # make a new connection
-                    continue
-
+            user_following_collector = UserFollowingCollector()
+            user_following_collector.add_user_friends_ids(user=user, twitter_app=twitter_app)
             user_queue.task_done()
 
 
+# Load the user profiles
 user_profile_collector = UserProfileCollector()
 user_profiles = user_profile_collector.load_user_profiles_as_list()
 
@@ -72,7 +53,7 @@ for app_type in ['normal', 'academic']:
     worker = threading.Thread(target=add_user_friends_ids, args=(user_queue, app_type), daemon=True)
     worker.start()
 
-for i in range(10):
+for i in range(32):
     print("adding user", user_profiles[i].user_name)
     user_queue.put(user_profiles[i])
 
