fea17718934b035677ddb573ca31363ab6333d59
Gustav Gyrst <ggyrst@gmail.com>
2022-10-21 15:25:13 +0200

    hpc doc and sbatch_output dir with log over past runs

---
+++ sbatch_outputs/create_fugazi_env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# Run this file simply running ./create_fugazi_env.sh
+
+
+# Fetch conda from the HPC to your local user
+module load Anaconda3/2021.05
+
+# init conda in the bash
+conda init bash
+
+
+# Go to the tweet_collector directory where files environment.yml and requirement.txt are located
+cd tweet-collector/
+
+# Create the fugazi environment
+conda create -n fugazi environment.yml
+
+
+echo "let's go motherfuckers! - did it work? - otherwise give me a call - Gustav"
