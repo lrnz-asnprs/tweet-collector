a43f97d501d306f36866ef0d8b051e2676daa1d8
gugy <ggyrst@gmail.com>
2022-11-03 15:49:56 +0100

    Implementation of the V1 PolFollowScorer - first part / count pols followed - missing scoring method

---
+++ drivers/worldview/PolFollowScorer.py
@@ -0,0 +1,69 @@
+from drivers.configs.directories import Directories
+import pickle, os
+import pandas as pd
+dir = Directories()
+
+
+class PolFollowScorer():
+    
+    def __init__(self, fake=True):
+        self.dir = Directories()
+        
+        #Depending on whether one wants the data for the fake or true user sample.
+        if fake:
+            self.directory = self.dir.FAKE_USERS_FOLLOWING_PATH
+        else:
+            self.directory = self.dir.TRUE_USERS_FOLLOWING_PATH
+        
+        
+        self.politicians = self.__read_politicians(dir.DATA_PATH / "legislators", "legislators-current-twIDs.csv")
+        self.politicians_twitter_ids = self.__read_politicians_ids(dir.DATA_PATH / "legislators", "legislators-current-twIDs.csv")
+        self.users_politicians_followed = {}
+    
+    def read_user_followings(self, path, filename):
+        with open(path / filename, "rb") as f:
+            users_following_ids = pickle.load(f)
+        return users_following_ids
+    
+    def __read_politicians_ids(self, path, filename):
+        pol_twitter_ids = set(pd.read_csv(path / filename).twitter_ids)
+        return pol_twitter_ids
+    
+    def __read_politicians(self, path, filename):
+        self.politicians = {}
+        for row in pd.read_csv(path / filename).iterrows():
+            row = row[-1]
+            self.politicians[row['twitter_ids']] = [row.full_name, row.last_name, row.party, row.gender, row.state, row.type]
+        
+        return self.politicians
+        
+    
+    def __get_users_followings(self):
+        directory = self.directory
+        users_following_pickles = []
+
+        for file in os.listdir(directory):
+            with open(directory / file, "rb") as f:
+                users_following_ids = pickle.load(f)
+                users_following_pickles.append(users_following_ids)
+        
+        return users_following_pickles
+    
+    
+    def find_politicians_followed(self):
+        """The mothership method that finds what politicians each user is following.
+        """
+        users_following_pickles = self.__get_users_followings()
+        
+        for pickle in users_following_pickles:
+            
+            for key, user in pickle.items():
+                self.users_politicians_followed[key] = []
+                for following in user.following_ids:
+                    if following in self.politicians_twitter_ids:
+                        self.users_politicians_followed[key].append(following)
+        
+        return self.users_politicians_followed
+                    
+    def describe_users(self):
+        return pd.DataFrame(self.users_politicians_followed).describe()
--- drivers/worldview/worldview.md
+++ drivers/worldview/worldview.md
@@ -3,20 +3,20 @@
 ## news_sources 
 
 ### 60 news sources Dem/Rep Trust Scores
-
 File: dem_rep_trustscores.json
+script: NewsScorer60.py
 Content: Measure for how much each 1000 american survey respondants trust in 60 common news medias
 Source: from source of the Elites paper
 
-### Dem/Rep News Sources
+### Dem/Rep News Sources Ideology Scores
 
 File: dem_rep_news.json
+Script: Newsscorer250.py
 Content: 250 news sources labelled from on a scale from: strong democratic - lean democratic - centrist - lean republican - strong republican
 Source: Aarhus paper on what drives spreading of Fake News.
 
 
 ### Dem/Rep Leaning Fake News
-
 File: dem_rep_fakenews_sources.json
 Content: ~40 dem/rep pro fakenews sources.
 Source: Aarhus paper. 
